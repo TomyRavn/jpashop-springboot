98adaa90270108b0290b0996cecc5cc4df61f8aa
TomyRavn <godqwer123@naver.com>
2022-01-03 14:17:54 +0900

    상품 주문, 목록, 검색, 취소(전체 기능 구현 완료)

---
+++ src/main/java/jpabook/jpashop/controller/OrderController.java
@@ -0,0 +1,64 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.service.ItemService;
+import jpabook.jpashop.service.MemberService;
+import jpabook.jpashop.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+    private final MemberService memberService;
+    private final ItemService itemService;
+
+    @GetMapping("/order")
+    public String createForm(Model model){
+        List<Member> members = memberService.findMembers();
+        List<Item> items = itemService.findItems();
+
+        model.addAttribute("members", members);
+        model.addAttribute("items", items);
+
+        return "order/orderForm";
+    }
+
+    @PostMapping("/order")
+    public String order(@RequestParam("memberId") Long memberId,
+                        @RequestParam("itemId") Long itemId,
+                        @RequestParam("count") int count){
+
+        //영속성 컨텍스트가 존재하는 상태에서 처리하고, 비즈니스 로직의 집중화 등을 위해
+        //컨트롤러에서는 식별자만 넘기는 방식으로 처리하고,
+        //service 등 핵심 비즈니스 로직이 포함된 내부에서 처리하는 것을 권장 (***)
+
+        orderService.order(memberId, itemId, count);
+        return "redirect:/orders";
+
+    }
+
+    @GetMapping("/orders")
+    public String orderList(@ModelAttribute("orderSearch")OrderSearch orderSearch, Model model){
+        List<Order> orders = orderService.findOrders(orderSearch);
+        model.addAttribute("orders", orders);
+
+        return "order/orderList";
+    }
+
+    @PostMapping("/orders/{orderId}/cancel")
+    public String cancelOrder(@PathVariable("orderId") Long orderId){
+        orderService.cancelOrder(orderId);
+        return "redirect:/orders";
+    }
+
+}
